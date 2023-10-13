c9a8d85b3baf197c8bc04f701142cde85cddf010
vivek <kumarvivek25101@gmail.com>
2023-10-14 01:25:19 +0530

    rese

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ServiceWeb">
       <div className="App">
         <Header />
         <Routes>
